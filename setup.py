29e14f38a301e16cc2f37417c5c4a27fcc53044b
Stanislau Fink <ravenhollm@gmail.com>
2018-06-11 18:53:13 +0300

    means for setup added

---
+++ setup.py
@@ -0,0 +1,10 @@
+
+from setuptools import setup, find_packages
+
+setup(name='denoiser',
+      version='0.1',
+      author='Stanislau Fink',
+      author_email='stanislau.fink@gmail.com',
+      description='Denoises image with known noise bitmask',
+      zip_safe=False,
+      packages=find_packages())
