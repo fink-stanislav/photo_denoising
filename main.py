db969ee089e35e93eca7a60f9dcd1acfc449cd30
Stanislau Fink <stanislau.fink@gmail.com>
2019-01-07 13:40:27 +0000

    Command line interface added

---
+++ main.py
@@ -0,0 +1,40 @@
+import os
+
+import denoising.image_utils as iu
+from denoising.deep_image_prior import Denoiser
+from os.path import join as jp
+
+
+if __name__ == '__main__':
+
+    steps = 100000000
+
+    base_folder = jp('/', 'home', 'developer', 'photo_restoration', 'data')
+    results_index = 3
+
+    result_folder = jp(base_folder, 'results_{}'.format(str(results_index)))
+    if os.path.isdir(result_folder):
+        if len(os.listdir(result_folder)) > 0:
+            raise ValueError('Folder "{}" already exists'.format(result_folder))
+    else:
+        os.mkdir(result_folder)
+
+    crops_img_index = 4
+    crop_index = 1
+    use_crops = True
+
+    mask_filename = 'mask.png'
+    image_filename = 'noisy_image.png'
+
+    if use_crops:
+        base_folder = jp(base_folder, 'crops_{}'.format(str(crop_index)))
+        mask_path = jp(base_folder, 'mask', 'IMG-{}.png'.format(str(crops_img_index)))
+        noisy_path = jp(base_folder, 'source', 'IMG-{}.png'.format(str(crops_img_index)))
+    else:
+        mask_path = jp(base_folder, mask_filename)
+        noisy_path = jp(base_folder, image_filename)
+
+    noisy = iu.file_to_tensor(noisy_path)
+    mask = iu.file_to_tensor(mask_path)
+
+    Denoiser(result_folder, num_steps=steps).denoise(mask, noisy)
